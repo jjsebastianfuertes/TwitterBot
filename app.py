32c7c6626f8d9f540647549d3322693da19d9e06
jjsebastianfuertes <jjsebastianfuertes@gmail.com>
2019-07-08 18:16:54 -0500

    set up

---
+++ app.py
@@ -0,0 +1,19 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+
+
+class TwitterBot:
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+        self.bot = webdriver.Firefox()
+
+    def login(self):
+        bot = self.bot
+        bot.get('https://twitter.com/')
+        time.sleep(3)
+
+
+ed = TwitterBot('storyoftheday@yahoo.com', 'sebas1234')
+ed.login
