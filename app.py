1d2e9c44cdd93abdd6c8512232a0fd9e20a065f5
jjsebastianfuertes <jjsebastianfuertes@gmail.com>
2019-07-08 19:29:20 -0500

    log in logic with selenium

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
+"""
+bot = webdriver.Firefox()
+bot.get("https://twitter.com/")
+
+ """
 
 
 class TwitterBot:
@@ -11,9 +16,17 @@ class TwitterBot:
 
     def login(self):
         bot = self.bot
-        bot.get('https://twitter.com/')
+        bot.get("https://twitter.com/")
+        time.sleep(3)
+        email = bot.find_element_by_class_name('email-input')
+        password = bot.find_element_by_name('session[password]')
+        email.clear()
+        password.clear()
+        email.send_keys(self.username)
+        password.send_keys(self.password)
+        password.send_keys(Keys.RETURN)
         time.sleep(3)
 
 
-ed = TwitterBot('storyoftheday@yahoo.com', 'sebas1234')
-ed.login
+ed = TwitterBot('jjsebastianfuertes@gmail.com', 'Aprendizcruel1995')
+ed.login()
